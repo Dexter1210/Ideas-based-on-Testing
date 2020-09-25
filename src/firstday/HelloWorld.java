d446885adbdf71b3c3e6437e24d8d95f7951b2fa
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-25 15:46:35 +0530

    Login by selenium

--- src/firstday/HelloWorld.java
+++ src/firstday/HelloWorld.java
@@ -1,5 +1,6 @@
 package firstday;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,16 +9,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class HelloWorld {
 	
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Hello World");
-		System.setProperty("webdriver.chrome.driver", "C:\\3dplmtraining\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("http://www.google.com");
-		WebElement searchfield=driver.findElement(By.name("q"));
-		searchfield.sendKeys("HDFC Bank");
-		searchfield.click();
-		Thread.sleep(200000);
-		driver.close();
+
+System.out.println("Hello World");
+  System.setProperty("webdriver.chrome.driver", "C:\\3dplmtraining\\chromedriver.exe");
+  WebDriver driver=new ChromeDriver();
+  
+  driver.get("http://www.demo.guru99.com/test/newtours/");
+  WebElement searchField=driver.findElement(By.name("userName"));
+  WebElement searchField1=driver.findElement(By.name("password"));
+  WebElement searchField2=driver.findElement(By.name("submit"));
+  searchField.sendKeys("mercury");
+  searchField1.sendKeys("mercury");
+  searchField2.click();
+  Thread.sleep(5000);
+  driver.close();
+
 	}
 
 }
